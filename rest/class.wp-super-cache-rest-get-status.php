4cc1abc849d5b86eda31439d303a47a6168d2f9a
Donncha O Caoimh <donncha@automattic.com>
2017-06-20 10:53:46 +0000

    Change status messages from objects to bools

--- rest/class.wp-super-cache-rest-get-status.php
+++ rest/class.wp-super-cache-rest-get-status.php
@@ -35,31 +35,13 @@ class WP_Super_Cache_Rest_Get_Status extends WP_REST_Controller {
 		include( $wp_cache_config_file );
 
 		if ( false == $cache_enabled ) {
-			$status[ 'preload_disabled_cache_off' ] = array(
-				'type' => 'warning',
-				'message' => __(
-					'Preloading is disabled as caching is disabled',
-					'wp-super-cache'
-				),
-			);
+			$status[ 'preload_disabled_cache_off' ] = true;
 		}
 		if ( false == $super_cache_enabled ) {
-			$status[ 'preload_disabled_supercache_off' ] = array(
-				'type' => 'warning',
-				'message' => __(
-					'Preloading is disabled as supercaching is disabled',
-					'wp-super-cache'
-				),
-			);
+			$status[ 'preload_disabled_supercache_off' ] = true;
 		}
 		if ( true === defined( 'DISABLESUPERCACHEPRELOADING' ) ) {
-			$status[ 'preload_disabled_by_admin' ] = array(
-				'type' => 'warning',
-				'message' => __(
-					'Preloading is disabled by the administrator of your site',
-					'wp-super-cache'
-				),
-			);
+			$status[ 'preload_disabled_by_admin' ] = true;
 		}
 	}
 
@@ -74,13 +56,7 @@ class WP_Super_Cache_Rest_Get_Status extends WP_REST_Controller {
 		if ( $cache_enabled && !$wp_cache_mod_rewrite ) {
 			$scrules = trim( implode( "\n", extract_from_markers( trailingslashit( get_home_path() ) . '.htaccess', 'WPSuperCache' ) ) );
 			if ( $scrules != '' ) {
-				$status[ 'php_mod_rewrite' ] = array(
-					'type' => 'warning',
-					'message' => __(
-						'Notice: PHP caching enabled but Supercache mod_rewrite rules detected. Cached files will be served using those rules. If your site is working ok, please ignore this message.  Otherwise, you can edit the .htaccess file in the root of your install and remove the SuperCache rules.',
-						'wp-super-cache'
-					),
-				);
+				$status[ 'php_mod_rewrite' ] = true;
 			}
 		}
 	}
@@ -92,13 +68,7 @@ class WP_Super_Cache_Rest_Get_Status extends WP_REST_Controller {
 		global $wp_cache_config_file;
 
 		if ( ! is_writeable_ACLSafe( $wp_cache_config_file ) ) {
-			$status['cache_disabled'] = array(
-				'type' => 'warning',
-				'message' => __(
-					'Read Only Mode. Configuration cannot be changed.',
-					'wp-super-cache'
-				),
-			);
+			$status['cache_disabled'] = true;
 		}
 	}
 
@@ -107,21 +77,9 @@ class WP_Super_Cache_Rest_Get_Status extends WP_REST_Controller {
 	 */
 	protected function add_compression_status( & $status ) {
 		if ( defined( 'WPSC_DISABLE_COMPRESSION' ) ) {
-			$status['compression_disabled'] = array(
-				'type' => 'warning',
-				'message' => __(
-					'Compression disabled by a site administrator.',
-					'wp-super-cache'
-				),
-			);
+			$status['compression_disabled'] = true;
 		} elseif ( false == function_exists( 'gzencode' ) ) {
-			$status['compression_disabled'] = array(
-				'type' => 'warning',
-				'message' => __(
-					'Warning! Compression is disabled as gzencode() function was not found.',
-					'wp-super-cache'
-				),
-			);
+			$status['compression_disabled'] = true;
 		}
 	}
 
@@ -142,33 +100,15 @@ class WP_Super_Cache_Rest_Get_Status extends WP_REST_Controller {
 		extract( wpsc_get_htaccess_info() );
 
 		if ( $scrules != $rules ) {
-			$status[ 'mod_rewrite_rules' ] = array(
-				'type' => 'warning',
-				'message' => __(
-					'The rewrite rules required by this plugin have changed or are missing. Cache files will still be served by PHP.',
-					'wp-super-cache'
-				),
-			);
+			$status[ 'mod_rewrite_rules' ] = true;
 		}
 		$got_rewrite = apache_mod_loaded( 'mod_rewrite', true );
 		if ( $wp_cache_mod_rewrite && false == apply_filters( 'got_rewrite', $got_rewrite ) ) {
-			$status['mod_rewrite_missing'] = array(
-				'type' => 'warning',
-				'message' => __(
-					'The mod_rewrite module has not been detected. Cache files will still be served by PHP.',
-					'wp-super-cache'
-				),
-			);
+			$status[ 'mod_rewrite_missing' ] = true;
 		}
 
 		if ( !is_writeable_ACLSafe( $home_path . ".htaccess" ) ) {
-			$status[ 'htaccess_ro' ] = array(
-				'type' => 'warning',
-				'message' => sprintf( __(
-					'The .htaccess file is readonly and cannot be updated. Cache files will still be served by PHP. See <a href="%s">Changing File Permissions</a> on WordPress.org for help on fixing this.',
-					'wp-super-cache'
-				), 'https://codex.wordpress.org/Changing_File_Permissions' ),
-			);
+			$status[ 'htaccess_ro' ] = true;
 		}
 	}
 }
