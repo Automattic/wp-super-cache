1363bb43bf2d247524c73efdc833a2dcd5aa6edf
Donncha O Caoimh <donncha@automattic.com>
2019-05-12 13:19:26 +0000

    Remove this line as it's completely incorrect.
    For more:
    https://wordpress.org/support/topic/instruction-to-modify-cachehome-is-not-clear/

--- wp-cache.php
+++ wp-cache.php
@@ -2620,7 +2620,6 @@ function wp_cache_check_link() {
 		echo "<li>" . sprintf( __( 'Make %1$s writable using the chmod command through your ftp or server software. (<em>chmod 777 %1$s</em>) and refresh this page. This is only a temporary measure and you&#8217;ll have to make it read only afterwards again. (Change 777 to 755 in the previous command)', 'wp-super-cache' ), WP_CONTENT_DIR ) . "</li>";
 		echo "<li>" . sprintf( __( 'Refresh this page to update <em>%s/advanced-cache.php</em>', 'wp-super-cache' ), WP_CONTENT_DIR ) . "</li></ol>";
 		echo sprintf( __( 'If that doesn&#8217;t work, make sure the file <em>%s/advanced-cache.php</em> doesn&#8217;t exist:', 'wp-super-cache' ), WP_CONTENT_DIR ) . "<ol>";
-		printf( __( '<li>Open <em>%1$s</em> in a text editor.</li><li>Change the text <em>CACHEHOME</em> to <em>%2$s</em></li><li> Save the file and copy it to <em>%3$s</em> and refresh this page.</li>', 'wp-super-cache' ), $wp_cache_file, WPCACHEHOME, $wp_cache_link );
 		echo "</ol>";
 		echo "</div>";
 		return false;
