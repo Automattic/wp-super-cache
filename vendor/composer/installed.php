20e8991f8394a5a0b280796a8744e4152275b69c
eAdnan007 <eAdnan007@users.noreply.github.com>
2022-12-27 09:39:25 +0000

    Image Guide: Update versions (#28082)
    * Update changelog
    
    * Remove release branch prefix from image-guide
    
    * Bump version number
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3786471934

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-device-detection' => array(
             'pretty_version' => '1.4.22',
             'version' => '1.4.22.0',
-            'reference' => '8fea965d396581110cc658427fa0e2eebf88f3fe',
+            'reference' => '9a06f07374eddb497e3fbea51385b38e97c19ce0',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
