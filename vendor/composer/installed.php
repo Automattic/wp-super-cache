0491b6cf0a563bb2498f7849f455bc0e10f45531
thingalon <thingalon@users.noreply.github.com>
2023-01-23 06:57:08 +0000

    Updated package versions for automattic/wp-super-cache
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3983971347

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-device-detection' => array(
-            'pretty_version' => '1.4.23',
+            'pretty_version' => 'v1.4.23',
             'version' => '1.4.23.0',
-            'reference' => '047c202272c0c2b4848ec62a1596e7c111195c20',
+            'reference' => '3fbb189538791c5a6fb4793536718307d44c1886',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
