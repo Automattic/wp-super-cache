161716b8e54d410fdea53982f5a0ffc132a6c4f6
samiff <samiff@users.noreply.github.com>
2023-02-20 21:45:59 +0000

    Jetpack: backport 11.9-a.5 changelog (#29056)
    * Jetpack: 11.9-a.5 changelog
    
    * Jetpack: init 11.9-a.6
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4227191789

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-device-detection' => array(
-            'pretty_version' => '1.4.24-alpha',
-            'version' => '1.4.24.0-alpha',
-            'reference' => '507115df30fceb66c2c976af1c694ca932add3f1',
+            'pretty_version' => '1.4.24',
+            'version' => '1.4.24.0',
+            'reference' => '50f5e232970191bc45ddd1afaaba6ff70be7e768',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
