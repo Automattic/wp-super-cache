1785e16ce13cfd95de2e0d78f170b1e390f924aa
anomiex <anomiex@users.noreply.github.com>
2023-02-20 09:34:29 +0000

    Remove remnants of automated code coverage support (#28982)
    PR #28542 removed the calls to the coverage scripts (except for one
    line), but didn't update the documentation, remove the scripts
    themselves, or adjust `jetpack generate` to stop generating those
    scripts. This does all that.
    
    It does not remove the PHPUnit or Jest definitions of which files should
    be looked at for coverage, or any `@codeCoverageIgnore` directives, or
    the `test-coverage-html` script in packages/waf, as those may still be
    useful for people manually looking at coverage locally.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4221844145

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-device-detection' => array(
-            'pretty_version' => '1.4.23',
-            'version' => '1.4.23.0',
-            'reference' => '047c202272c0c2b4848ec62a1596e7c111195c20',
+            'pretty_version' => '1.4.24-alpha',
+            'version' => '1.4.24.0-alpha',
+            'reference' => '507115df30fceb66c2c976af1c694ca932add3f1',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
