292a06fafd336c3b0c6a3078b702516014ef725e
jeherve <jeherve@users.noreply.github.com>
2023-04-10 10:08:58 +0000

    Packages: add Autoloader suggestion (#29988)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4656408419

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-device-detection' => array(
-            'pretty_version' => '1.4.24',
-            'version' => '1.4.24.0',
-            'reference' => '50f5e232970191bc45ddd1afaaba6ff70be7e768',
+            'pretty_version' => '1.4.25-alpha',
+            'version' => '1.4.25.0-alpha',
+            'reference' => '3ced6da9ee10563fb560f651b840d2f22f6b7252',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
