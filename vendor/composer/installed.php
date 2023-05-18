fd3e4076b0b387fccb184467eeb3078d09671808
dhasilva <dhasilva@users.noreply.github.com>
2023-05-18 13:37:49 +0000

    AI Assistant: Add treatment for error when prompt is not understood (#30778)
    * add error notice when prompt is not understood
    
    * changelog
    
    * simplify error parsing logic
    
    * update error message
    
    * remove dismiss button from notice and auto-dismiss on input change
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5014327561

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-device-detection' => array(
             'pretty_version' => '1.4.25',
             'version' => '1.4.25.0',
-            'reference' => '949b4a37dc7bf8feb9cfe7c3eea959d4f4dfb1e4',
+            'reference' => 'c2d31fa5bea7617787b02ded6b3b46bc12f0e93b',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
