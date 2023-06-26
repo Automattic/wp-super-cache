a030bb80100607d79b57847a6068ada7ea45627d
anomiex <anomiex@users.noreply.github.com>
2023-06-26 17:18:16 +0000

    phpcs: DRY in .phpcs.dir.phpcompatibility.xml (#31573)
    For cases where we want to ignore a PHPCompatibility rule, we need to
    ignore it in both `.phpcs.dir.xml` and `.phpcs.dir.phpcompatibility.xml`.
    
    But there's no need to do it via copy-paste. We can have
    `.phpcs.dir.xml` load `.phpcs.dir.phpcompatibility.xml` so we only have
    to have the ignores in one place.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5380889610

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-device-detection' => array(
             'pretty_version' => '1.4.26',
             'version' => '1.4.26.0',
-            'reference' => '760e137a59d443db0574872755c301a72fa0aba8',
+            'reference' => '974d103e0c4a076cd3e3c5c5fa85b65dea3cf89d',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
