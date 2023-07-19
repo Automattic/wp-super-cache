c8b3555283990797220d246fe0ad5a31feeb30b0
anomiex <anomiex@users.noreply.github.com>
2023-07-19 17:14:44 +0000

    cli: Check node, php, and pnpm versions too (#31918)
    And also better handle the case where the program is not installed.
    
    Also fix the handler for "forgot to `pnpm install`", it wasn't working
    since #23966 added some unguarded `import`s in the `bin/jetpack.js`
    entry point.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5601946967

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'automattic/wp-super-cache',
+        'pretty_version' => 'dev-trunk',
+        'version' => 'dev-trunk',
+        'reference' => NULL,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'automattic/jetpack-device-detection' => array(
+            'pretty_version' => '1.4.26',
+            'version' => '1.4.26.0',
+            'reference' => 'bf77c8b509801c531708038c56a3a79d08358531',
+            'type' => 'jetpack-library',
+            'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'automattic/wp-super-cache' => array(
+            'pretty_version' => 'dev-trunk',
+            'version' => 'dev-trunk',
+            'reference' => NULL,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
