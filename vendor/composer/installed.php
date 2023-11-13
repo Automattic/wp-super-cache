27df019f1a49db959101dde268278b90fb698a00
zinigor <zinigor@users.noreply.github.com>
2023-11-13 16:36:20 +0000

    Backport mu-wpcom-plugin 1.7.10, jetpack 12.9-a.3 Changes (#34089)
    * Update changelogs
    
    * Changelog edits.
    
    * Update versions
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6852775762

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-device-detection' => array(
-            'pretty_version' => '1.5.0-alpha.1699885257',
-            'version' => '1.5.0.0-alpha1699885257',
-            'reference' => '5c3681d320dcedec693fc9b57d0c7ee4fdd92d98',
+            'pretty_version' => '1.5.1-alpha.1699892604',
+            'version' => '1.5.1.0-alpha1699892604',
+            'reference' => 'ce77bf3189a30743e663c2107ab82b63d697e5b1',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
