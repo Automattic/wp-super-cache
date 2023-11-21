3a7be6efd8bcd83e35c7f92fa9ec389b24c33756
jeherve <jeherve@users.noreply.github.com>
2023-11-21 17:03:50 +0000

    Remove unnecessary `function_exists()` checks (#34222)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6947021022

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-device-detection' => array(
             'pretty_version' => '2.0.1-alpha.1700575066',
             'version' => '2.0.1.0-alpha1700575066',
-            'reference' => 'e0447994a18c25d509d25318e80328ab301f5385',
+            'reference' => 'e7d268322e6dda45b60f67d6f053efd47f261dd1',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
