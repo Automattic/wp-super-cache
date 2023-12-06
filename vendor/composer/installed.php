467574064d925098072ba95f57ce458eca5dfb3e
anomiex <anomiex@users.noreply.github.com>
2023-12-06 17:40:35 +0000

    Update dependency wikimedia/testing-access-wrapper to v3 (#34492)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7118052079

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-device-detection' => array(
             'pretty_version' => '2.0.1',
             'version' => '2.0.1.0',
-            'reference' => 'd6cfc8b644cbece24fb6631b10d99d04b98893e3',
+            'reference' => '03212ed99e63e624f141ff05f4d83f610bfe2cf9',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
