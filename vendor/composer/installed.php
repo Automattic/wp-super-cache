5d8347eeaf57ea4bccc51afaea9a76e52c3ed172
anomiex <anomiex@users.noreply.github.com>
2023-12-11 20:49:48 +0000

    Backport mu-wpcom-plugin 2.0.5, jetpack 13.0-a.1 Changes (#34585)
    * Add changelogs
    
    * Changelog edits.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7173088707

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-device-detection' => array(
             'pretty_version' => '2.0.1',
             'version' => '2.0.1.0',
-            'reference' => '03212ed99e63e624f141ff05f4d83f610bfe2cf9',
+            'reference' => 'c904e39929821d06575bd1185dc1c2a871d1a9cb',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
