5475df7c026c64547d5ac1dc282b9a967941b5b8
anomiex <anomiex@users.noreply.github.com>
2024-03-15 15:26:43 +0000

    Update dependency mediawiki/mediawiki-codesniffer to v43 (#36408)
    * Update dependency mediawiki/mediawiki-codesniffer to v43
    * Update snapshots
      Changed due to fix of a wrapping bug in phpcs
    * Update root composer.lock
    * Fix sniffs newly reported by phpcs 3.8.1
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8298410577

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-device-detection' => array(
-            'pretty_version' => '2.1.1',
-            'version' => '2.1.1.0',
-            'reference' => '59355ce8e0b4b90c2b589270ef8e328c29fdb346',
+            'pretty_version' => '2.1.2-alpha.1710515639',
+            'version' => '2.1.2.0-alpha1710515639',
+            'reference' => '8010af1f9fdb89ae4a5294b0426a5043e931697d',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
