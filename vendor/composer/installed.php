9929b96ad2a76199fb490028945524037d03c252
anomiex <anomiex@users.noreply.github.com>
2024-04-09 11:56:08 +0000

    phan: Rework how stubs are specified (#36791)
    In preparation for adding more specialized stubs, add a way for configs
    to specify which they want by keyword rather than having to do file
    paths relative to the root.
    
    As part of that, `'is_wordpress' => false` goes away in favor of
    overriding the default list of stubs to load.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8615103856

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-device-detection' => array(
             'pretty_version' => '2.1.3-alpha.1712563319',
             'version' => '2.1.3.0-alpha1712563319',
-            'reference' => 'a9b7de14c343d0351f22ffce79d397bc5987e039',
+            'reference' => 'feab2b6ee41ad0af932325e1360912aa1c9fe549',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
