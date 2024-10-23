1d6e8301b84e3e139a88f4a95d17510d41781c31
anomiex <anomiex@users.noreply.github.com>
2024-10-23 20:59:21 +0000

    changlogger: When amending, do not preserve default link (#39868)
    When doing `changelogger write --amend`, we normally preserve the
    prologue, epilogue, and link unless these are overridden on the command
    line.
    
    An edge case is when you're amending version 1.2.3-beta to turn it into
    1.2.3 release, and the link is the default link like
    `https://example.com/diff/1.2.2...1.2.3-beta`. In this situation we
    don't want to keep that link, we want to replace it with the new default
    `https://example.com/diff/1.2.2...1.2.3`.
    
    Also, when building changelogger, we have to install before trying to run it.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11487859862
    
    Upstream-Ref: Automattic/jetpack@07e18c23a06b9bc8afe4a5323c5de4b72ce2354b

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-device-detection' => array(
             'pretty_version' => '2.1.5',
             'version' => '2.1.5.0',
-            'reference' => '92b614e873d90f69847bc143c21b8a997e0c0428',
+            'reference' => '77353da5f11350060991595061a97d756ddb43e2',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
