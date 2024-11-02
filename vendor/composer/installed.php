6a5108e606f967727d95502ad2d8eb32dbb3c52b
anomiex <anomiex@users.noreply.github.com>
2024-11-02 12:00:36 +0000

    coverage: Switch to raw coverage files (#40013)
    It turns out that there really aren't any really good generic coverage
    reporting tools, the best you can get is something that highlights
    covered lines without function/method stats, branch info, or anything
    else fancy.
    
    To provide useful coverage output, we're going to look instead at
    generating separate coverage dashboards for PHP (PHPUnit) and JS
    (jest/istanbul), each generated by those tools so we can get all the
    useful info possible. To do that, we'll need all the coverage commands
    to generate the tools' raw data formats rather than a lossy format like
    clover.
    
    As part of this conversion, we're adding `jest.config.cjs` everywhere so
    we can consistently set `collectCoverageFrom`. By default the base
    config will include non-test files under `src/`. Various packages need
    additional dirs such as `components/`, and a root-level `index.jsx` or
    `global.d.ts` seems common too. Then, since we have config files
    everywhere, we can move `coverageDirectory` and `coverageReporters` into
    the configs too.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11642430412
    
    Upstream-Ref: Automattic/jetpack@161c7a68f045ceb3ff8dad73ed308a9b53738c36

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-device-detection' => array(
-            'pretty_version' => '2.1.6-alpha.1730371893',
-            'version' => '2.1.6.0-alpha1730371893',
-            'reference' => 'ce2d88faa4e94f8d11bddafcbf7d707d95c1eb75',
+            'pretty_version' => '2.1.6-alpha.1730548010',
+            'version' => '2.1.6.0-alpha1730548010',
+            'reference' => '460f09d797ba050711d2bc3a813742d427a8660e',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
