961ab356684208c40065464ea0ac331ac1f9f829
zinigor <zinigor@users.noreply.github.com>
2024-11-25 17:47:24 +0000

    Changelog and readme.txt edits. (#40333)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12015553649
    
    Upstream-Ref: Automattic/jetpack@e34d1bcfbd7cafe30ca907681c9de5830df047c0

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-device-detection' => array(
             'pretty_version' => '3.0.0',
             'version' => '3.0.0.0',
-            'reference' => 'cdf3502969a67b22971d5e20457113490558f55f',
+            'reference' => '88cb84f1b631353fa20891c0cded95f1555c1fa6',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
