78f5a265a72023a9af882237583590b1869f7b2c
anomiex <anomiex@users.noreply.github.com>
2025-02-20 15:50:09 +0000

    changelogger, stub-generator: Simplify bin by dropping Composer <2.2 support (#41876)
    Composer 2.2 added a global `$_composer_autoload_path` that points to
    the autoloader. We can simplify the bin files significantly by requiring
    this Composer version and using that constant instead of trying to guess
    where the autoloader lives.
    
    This also requires various tools be updated to run changelogger and
    stub-generator via the symlink added in `vendor/bin/` rather than trying
    to use `bin/` directly.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13438907848
    
    Upstream-Ref: Automattic/jetpack@95a448a72c4290fc9f57b4210259e438b032daec

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-device-detection' => array(
             'pretty_version' => '3.0.0',
             'version' => '3.0.0.0',
-            'reference' => '88cb84f1b631353fa20891c0cded95f1555c1fa6',
+            'reference' => 'da41451c1401be76130cea3e326bef27b8ae9ceb',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
