c23d199f28ad018d28c762cd3dd46c95749ae4d5
anomiex <anomiex@users.noreply.github.com>
2025-03-03 18:33:17 +0000

    phpunit: Rename package test classes for PHPUnit 11 compat (pt 1) (#42120)
    PHPUnit 10+ requires that test class names match the filename, PSR-4
    style. This PR renames the test classes and files in packages to end in
    `Test` or `_Test`, depending on whether the class names are following
    WordPress's underscore-naming convention or are using CamelCase, and
    removing redundant `WP_Test_` or `Test_` prefixes.
    
    This also adds our standard Jetpack-Tests ruleset to
    tests/.phpcs.dir.xml in the packages, and removes some redundant inline
    phpcs:ignore or phpcs:disable directives.
    
    Of note, changed test order in the Connection package broke a test.
    Adding some setup/teardown code to clear the affected singleton fixed
    it.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13637614579
    
    Upstream-Ref: Automattic/jetpack@327f25f4acf9a71d70b3f6883f26acc3377365a5

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-device-detection' => array(
-            'pretty_version' => '3.0.1',
-            'version' => '3.0.1.0',
-            'reference' => '5305964f13d655f5ba88d1e3cfb18d0e4608b09c',
+            'pretty_version' => '3.0.2-alpha.1741025873',
+            'version' => '3.0.2.0-alpha1741025873',
+            'reference' => 'd62e2655e500ca39ec4c8c5f9f6339694797cc02',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
