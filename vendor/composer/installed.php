dcfc9f8b0d330ee2a6be618a2eb0c6d633eb8606
dilirity <dilirity@users.noreply.github.com>
2025-03-05 13:13:36 +0000

    Changelog and readme.txt edits. (#42222)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13676530376
    
    Upstream-Ref: Automattic/jetpack@aff374a8994ab73af6f069899768be29812ec0e6

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-device-detection' => array(
-            'pretty_version' => '3.0.2-alpha.1741025873',
-            'version' => '3.0.2.0-alpha1741025873',
-            'reference' => 'd62e2655e500ca39ec4c8c5f9f6339694797cc02',
+            'pretty_version' => '3.0.2',
+            'version' => '3.0.2.0',
+            'reference' => '3a43dd00582f6cb6d9146ec2f2b3986e52d45ad4',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
