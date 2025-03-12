81daa29004e92cd72daf201934ceedc7017aa302
anomiex <anomiex@users.noreply.github.com>
2025-03-12 14:43:46 +0000

    phpunit: Use phpunit-select-config, and some cleanup (#42266)
    * Remove obsolete polyfills
    * gitignore `.phpunit.cache` by default
    * production-exclude `phpunit.xml.dist` and `phpunit.*.xml.dist` by default
    * production-exclude tests dirs
    * Use phpunit-select-config everywhere
    * cli: Support copying symlinks for `jetpack generate`
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13813825579
    
    Upstream-Ref: Automattic/jetpack@237f3f58cab74f799f6178c610f757e80343d7ac

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-device-detection' => array(
-            'pretty_version' => '3.0.2',
-            'version' => '3.0.2.0',
-            'reference' => '3a43dd00582f6cb6d9146ec2f2b3986e52d45ad4',
+            'pretty_version' => '3.0.3-alpha.1741789611',
+            'version' => '3.0.3.0-alpha1741789611',
+            'reference' => '49e607abf297bcffcec73530d5be59316efe996e',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
