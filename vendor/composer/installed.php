678c83f704e1b311ae4a520ed4269ce602a8ec5e
anomiex <anomiex@users.noreply.github.com>
2025-03-19 21:01:52 +0000

    Update PHPUnit stubs to PHPUnit 11.5 (#42591)
    This needs some changes to the munger:
    
    * `@psalm-` annotations seem mostly replaced with `@phpstan-`.
    * Some bad phpdocs have been fixed.
    
    This also calls for some cleanups of things deprecated or removed in
    PHPUnit 11:
    
    * `MockBuilder->addMethods()` is deprecated with no replacement. We
      often used this to "mock" stdClass; now, instead, we need to either
      use anonymous classes or mock a dummy interface.
    * `->getMockForAbstractClass()` is also deprecated. Can be replaced with
      `->onlyMethods( ... )->getMock()` but we have to manually list the
      needed methods.
    * `->getMockForTrait()` as well. Anonymous classes work well here.
    * `TestCase->returnValue()` is deprecated in favor of
      `$double->willReturn()`.
    * `TestCase->addWarning()` is gone. Seems to work replacing it with
      `trigger_error( E_USER_WARNING )`.
    * packages/masterbar: `Base_Admin_Menu_Test` has some of the same
      weirdness `WPcom_Admin_Menu_Test` did before #42504, just less
      obviously so.
    * plugins/jetpack: `Jetpack_Client_Server_Test` seems to have been
      creating unused mocks since #14043. Removed them.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13956378052
    
    Upstream-Ref: Automattic/jetpack@281a100a6e89c57e332b8b4584d8ff7f346e3bd5

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-device-detection' => array(
             'pretty_version' => '3.0.5-alpha.1742415897',
             'version' => '3.0.5.0-alpha1742415897',
-            'reference' => '948e5ec71b7ac43393c8ffef7aa9593910f9d6b3',
+            'reference' => '2abe1162e3d9a8dc5cb47659bf843ea24f9b827e',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
