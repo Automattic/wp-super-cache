c7de1d9a6eab4ad67313b6daec0bcab360422750
anomiex <anomiex@users.noreply.github.com>
2025-04-22 20:01:15 +0000

    phpcs: Fix and enable `Jetpack.PHPUnit.Attributes` sniff (#43175)
    Mostly this is the result of running phpcbf on files failing the sniff.
    
    Things needing manual cleanup were:
    
    * `projects/packages/image-cdn/tests/php/Image_CDN_Test.php`:
      Remove a broken `@covers` that had no space between the tag and class
      name.
    
    * `projects/plugins/jetpack/tests/php/core-api/WPCOM_REST_API_V2_Field_Controller_Test.php`:
      Fix a `@group` that was accidentally
      "rest-api\n\nphpcs:disable Generic.Files.OneObjectStructurePerFile.MultipleFound".
    
    * `projects/plugins/jetpack/tests/php/core-api/wpcom-fields/WPCOM_REST_API_V2_Attachment_VideoPress_Data_Test.php`:
    * `projects/plugins/jetpack/tests/php/sync/Jetpack_Sync_Queue_TestBase.php`:
      Move some `@group`s from the file comment to class comment.
    
    * `projects/plugins/jetpack/tests/php/Functions_OpenGraph_Test.php`:
    * `projects/plugins/jetpack/tests/php/general/Jetpack_Client_Server_Test.php`:
      Fix where it got confused about whether a comment was a file or class
      comment.
    
    * `projects/plugins/jetpack/tests/php/json-api/Jetpack_Json_Api_Plugins_Endpoints_Test.php`:
      Fix `@covers` for nonexistent class.
    
    * `projects/plugins/jetpack/tests/php/modules/shortcodes/trait.http-request-cache.php`:
    * `projects/plugins/jetpack/tests/php/trait-woo-tests.php`:
      Manually fix, since the sniff doesn't recognize the trait as a test
      class.
    
    * Disable `PHPCompatibility.Attributes.NewAttributes.PHPUnitAttributeFound` in phpcompatibility-dev run.
    
    `PHPCompatibility.Attributes.NewAttributes.Found` was already disabled.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14603283610
    
    Upstream-Ref: Automattic/jetpack@c49fe6886554654995d9d453e5191b2e5cb28a61

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-device-detection' => array(
-            'pretty_version' => '3.0.5',
-            'version' => '3.0.5.0',
-            'reference' => '03f4308b70945b46767101f139d095fd7f796ad0',
+            'pretty_version' => '3.0.6-alpha.1745351100',
+            'version' => '3.0.6.0-alpha1745351100',
+            'reference' => 'a14c799385e95f39635bf247497b5d97ab15ddda',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
