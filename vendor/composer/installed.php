16b07c2e68fb9de21ba884aefdf0019d2cd77346
chihsuan <chihsuan@users.noreply.github.com>
2025-06-06 09:16:35 +0000

    Fix PHPUnit "Data set x has more arguments " warnings (#43815)
    * Resolved PHPUnit warnings across multiple packages
    
    * changelog
    
    * Update projects/packages/waf/.phan/baseline.php
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15486868631
    
    Upstream-Ref: Automattic/jetpack@1b94aa0a52cd23c37200ba9d61124f79f2b7170f

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-device-detection' => array(
-            'pretty_version' => '3.0.6',
-            'version' => '3.0.6.0',
-            'reference' => 'f6b3328cadae3d425f9de7bd576da73462b3a5fb',
+            'pretty_version' => '3.0.7-alpha.1749200492',
+            'version' => '3.0.7.0-alpha1749200492',
+            'reference' => '49f8d3a9797577917c0f50e7a4223ae721f04e21',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
