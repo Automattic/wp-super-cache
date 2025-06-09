332f66adfdde4d9bf0501b6c38a2854f0afc9d9f
tbradsha <tbradsha@users.noreply.github.com>
2025-06-09 18:51:47 +0000

    Changelog and readme.txt edits. (#43857)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15541892088
    
    Upstream-Ref: Automattic/jetpack@d67bccc59af4dff6f066437794f6dab086b52fa6

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-device-detection' => array(
-            'pretty_version' => '3.0.8-alpha.1749489407',
-            'version' => '3.0.8.0-alpha1749489407',
-            'reference' => '2fb13fbc5fc872130991d21e1e046216da33c9a0',
+            'pretty_version' => '3.0.8',
+            'version' => '3.0.8.0',
+            'reference' => 'd444422e699935db9737c94b2cae4ba140ae0c7b',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
