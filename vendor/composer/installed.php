3f001f2ca1ee657c6bca4505c689305de90c2cb9
tbradsha <tbradsha@users.noreply.github.com>
2025-06-25 16:13:30 +0000

    Phan: Clean up `PhanRedundantCondition` violations (excluding jetpack and crm) (#44033)
    * packages/connection: PhanRedundantCondition
    
    * packages/device-detection: PhanRedundantCondition
    
    * packages/forms: PhanRedundantCondition
    
    * packages/jetpack-mu-wpcom: PhanRedundantCondition
    
    * packages/my-jetpack: PhanRedundantCondition
    
    * packages/search: PhanRedundantCondition
    
    * packages/status: PhanRedundantCondition
    
    * packages/sync: PhanRedundantCondition
    
    * packages/videopress: PhanRedundantCondition
    
    * packages/wp-js-data-sync: PhanRedundantCondition
    
    * plugins/boost: PhanRedundantCondition
    
    * plugins/super-cache: PhanRedundantCondition
    
    * plugins/vaultpress: PhanRedundantCondition
    
    * plugins/wpcomsh: PhanRedundantCondition
    
    * Add changelog
    
    * Trade a few Phan violations back for some compliant PHPCS
    
    * Update Phan baselines
    
    * Remove errant code
    
    * Update PHPDoc
    
    * Update projects/plugins/super-cache/wp-cache-phase2.php
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    * Explicitly initialize $cached_direct_pages early
    
    * Ensure we have what we need
    
    * Make Phan happier
    
    * Update condition to use int instead of bool
    
    * Update Phan baseline
    
    ---------
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15881245606
    
    Upstream-Ref: Automattic/jetpack@7f361d98b695f1ae6d8cf1ff830ce9c14abcb863

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-device-detection' => array(
-            'pretty_version' => '3.0.8',
-            'version' => '3.0.8.0',
-            'reference' => 'd444422e699935db9737c94b2cae4ba140ae0c7b',
+            'pretty_version' => '3.0.9-alpha.1750867098',
+            'version' => '3.0.9.0-alpha1750867098',
+            'reference' => '0261a4b754129b72c47526e651dcaeee55553fb8',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
