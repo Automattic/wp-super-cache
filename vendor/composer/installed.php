532935c2b638db718c7c91bfd42583b0468ff555
chihsuan <chihsuan@users.noreply.github.com>
2025-08-13 03:21:38 +0000

    Backport  packages/woocommerce-analytics 0.5.0 changes (#44770)
    * Bump packages/woocommerce-analytics 0.5.0
    
    * Bump other related packages
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16926384633
    
    Upstream-Ref: Automattic/jetpack@c147cd4b63c1de30c44cf11f28a3ec50a7cd998e

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-device-detection' => array(
             'pretty_version' => '3.0.9',
             'version' => '3.0.9.0',
-            'reference' => 'd85a430fcd1da596c27aae45bc985ce62f9bb56b',
+            'reference' => '1ede9d1c9c2a7079f0a7585d9cd7a9e94752c2ec',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
