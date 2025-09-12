f6b4758d16831ee35cd75ddba78eaefdad9d23d7
tbradsha <tbradsha@users.noreply.github.com>
2025-09-12 21:15:15 +0000

    Bump Phan to 5.5.1 (#45163)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17685854584
    
    Upstream-Ref: Automattic/jetpack@01dca7f95dbd32ffaa9cdc4b13632b66b9fb5f2b

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-device-detection' => array(
             'pretty_version' => '3.0.9',
             'version' => '3.0.9.0',
-            'reference' => '1ede9d1c9c2a7079f0a7585d9cd7a9e94752c2ec',
+            'reference' => '00dbe854b66f8349073bc2efccb11ad0c213ea1b',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
