3ef325a2fdb7152bf54bd47907ff165e4ec66491
vianasw <vianasw@users.noreply.github.com>
2025-10-30 19:24:33 +0000

    Forms: Add browser info to form responses detail view (#45674)
    * Add get_browser_display_name method
    
    The method returns a browser pretty named parsed from the user agent
    string or the string 'other' in case it's an unknown browser to us.
    
    * changelog
    
    * Add browser name and platform to form responses
    
    I've added the browser name and platform to the detail view of the form
    responses.
    
    * Add Other as display name catch all
    
    * Move Browser info from div to table
    
    * Fix composer lock issue
    
    * changelog
    
    * Fix static analysis issue
    
    * Fix unit tests
    
    * Update phan baseline
    
    * Return `Unknown browser` translated string...
    
    ...when we don't know the specific browser.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/18952224061
    
    Upstream-Ref: Automattic/jetpack@758e77e901d89c7668e8303691d1e7ac33486b1b

--- vendor/automattic/jetpack-device-detection/CHANGELOG.md
+++ vendor/automattic/jetpack-device-detection/CHANGELOG.md
@@ -9,6 +9,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 This is an alpha version! The changes listed here are not final.
 
+### Added
+- Device detection: added a method that returns the browser display name.
+
 ### Changed
 - Device detection: update bot detection list to include new AI and LLM crawler oriented bots.
 
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-device-detection' => array(
-            'pretty_version' => '3.3.0-alpha.1761816539',
-            'version' => '3.3.0.0-alpha1761816539',
-            'reference' => 'c4f5eb315b8b4d956bf7b3ee388e4e5a6b2d2766',
+            'pretty_version' => '3.3.0-alpha.1761851286',
+            'version' => '3.3.0.0-alpha1761851286',
+            'reference' => 'ce6219932c8bf1132842a842d059cefcdb969d29',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
             'aliases' => array(),
