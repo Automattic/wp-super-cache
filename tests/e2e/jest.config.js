aa5c0643aa955fde4eb4b359f75be8001b922963
Brandon Kraft <public@brandonkraft.com>
2026-04-07 17:54:07 -0500

    CI: add e2e test workflow (#1030)
    * ci: add e2e test workflow
    
    * fix: decouple e2e tests from Jetpack monorepo
    
    * fix: make apache2-wrapper.sh executable
    
    * fix: approve build scripts and fix jest config URL resolution
    
    * fix: inline tsconfig from jetpack-e2e-commons

--- tests/e2e/jest.config.js
+++ tests/e2e/jest.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath } from 'url';
 import { config as dotenvConfig } from 'dotenv';
 
 // Read .env file so variables are available in tests.
-dotenvConfig( { path: fileURLToPath( '.env', import.meta.url ) } );
+dotenvConfig( { path: fileURLToPath( new URL( '.env', import.meta.url ) ) } );
 
 export default {
 	preset: 'ts-jest',
